f37f693ddaa44268f4f9b71be9885611fcb9a438
evan <aspaasevan@gmail.com>
2020-05-15 08:21:42 -0700

    api up and running

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 
-const db = require('./data/dbConfig.js');
+const db = require('../data/dbConfig');
 
 const server = express();
 
