60d0dbfe248b718f186f2072b10a4ee3d4ca457d
evan <aspaasevan@gmail.com>
2020-05-15 08:24:17 -0700

    migrations and seeds setup complete

--- api/server.js
+++ api/server.js
@@ -8,8 +8,10 @@ const server = express();
 server.use(helmet());
 server.use(express.json());
 
-server.get("/", (req, res) => {
-  res.status(200).json({ api: "up" });
-})
+// server.get("/", (req, res) => {
+//   res.status(200).json({ api: "up" });
+// })
+
+
 
 module.exports = server;
